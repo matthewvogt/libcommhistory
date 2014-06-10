bfa4ff6c8c177d0f7b244d00fd622653dd13ee1a
John Brooks <john.brooks@jollamobile.com>
2014-06-10 09:35:54 -0600

    Use Recipient in Events

--- tests/perf_conversationmodel/conversationmodelperftest.cpp
+++ tests/perf_conversationmodel/conversationmodelperftest.cpp
@@ -132,7 +132,7 @@ void ConversationModelPerfTest::getEvents()
         e.setStartTime(when.addSecs(ei));
         e.setEndTime(when.addSecs(ei));
         e.setLocalUid(RING_ACCOUNT);
-        e.setRemoteUid(remoteUids.at(0));
+        e.setRecipients(Recipient(RING_ACCOUNT, remoteUids.at(0)));
         e.setFreeText(randomMessage(qrand() % 49 + 1)); // Max 50 words / message
         e.setIsDraft(false);
         e.setIsMissedCall(false);
